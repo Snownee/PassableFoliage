23d18af5049fd5b1ba56e5e532975494d20aba46
Snownee <1850986885@qq.com>
2020-05-04 12:33:15 +0800

    .

--- src/main/java/snownee/passablefoliage/mixin/MixinBlockState.java
+++ src/main/java/snownee/passablefoliage/mixin/MixinBlockState.java
@@ -9,35 +9,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
-import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 @Mixin(BlockState.class)
 public class MixinBlockState /*implements IForgeBlockState*/ {
 
-    @Inject(
-            at = @At(
-                "HEAD"
-            ), method = "getCollisionShape(Lnet/minecraft/world/IBlockReader;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/shapes/VoxelShape;", cancellable = true
-    )
-    private void pfoliage_getCollisionShape(IBlockReader worldIn, BlockPos pos, CallbackInfoReturnable<VoxelShape> info) {
-        //        if (((BlockState) (Object) this).isIn(PassableFoliage.PASSABLES)) {
-        //            info.setReturnValue(VoxelShapes.empty());
-        //        }
-    }
+    //    @Inject(
+    //            at = @At(
+    //                "HEAD"
+    //            ), method = "getCollisionShape(Lnet/minecraft/world/IBlockReader;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/math/shapes/VoxelShape;", cancellable = true
+    //    )
+    //    private void pfoliage_getCollisionShape(IBlockReader worldIn, BlockPos pos, CallbackInfoReturnable<VoxelShape> info) {
+    //        if (((BlockState) (Object) this).isIn(PassableFoliage.PASSABLES)) {
+    //            info.setReturnValue(VoxelShapes.empty());
+    //        }
+    //    }
 
-    @Inject(
-            at = @At(
-                "HEAD"
-            ), method = "getCollisionShape(Lnet/minecraft/world/IBlockReader;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/shapes/ISelectionContext;)Lnet/minecraft/util/math/shapes/VoxelShape;", cancellable = true
-    )
-    private void pfoliage_getCollisionShape(IBlockReader worldIn, BlockPos pos, ISelectionContext context, CallbackInfoReturnable<VoxelShape> info) {
-        //        if (((BlockState) (Object) this).isIn(PassableFoliage.PASSABLES)) {
-        //            info.setReturnValue(VoxelShapes.empty());
-        //        }
-    }
+    //    @Inject(
+    //            at = @At(
+    //                "HEAD"
+    //            ), method = "getCollisionShape(Lnet/minecraft/world/IBlockReader;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/shapes/ISelectionContext;)Lnet/minecraft/util/math/shapes/VoxelShape;", cancellable = true
+    //    )
+    //    private void pfoliage_getCollisionShape(IBlockReader worldIn, BlockPos pos, ISelectionContext context, CallbackInfoReturnable<VoxelShape> info) {
+    //        if (((BlockState) (Object) this).isIn(PassableFoliage.PASSABLES)) {
+    //            info.setReturnValue(VoxelShapes.empty());
+    //        }
+    //    }
 
     @Inject(at = @At("HEAD"), method = "isCollisionShapeOpaque", cancellable = true)
     private void pfoliage_isCollisionShapeOpaque(IBlockReader blockReaderIn, BlockPos blockPosIn, CallbackInfoReturnable<Boolean> info) {
