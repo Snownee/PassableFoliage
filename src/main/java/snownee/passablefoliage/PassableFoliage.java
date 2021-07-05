b09df0335495ad6ffd30ea06bc773dda50866dbe
Snownee <mjy523@126.com>
2021-07-05 20:18:42 +0800

    fix: #17

--- src/main/java/snownee/passablefoliage/PassableFoliage.java
+++ src/main/java/snownee/passablefoliage/PassableFoliage.java
@@ -1,5 +1,6 @@
 package snownee.passablefoliage;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -21,87 +22,93 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(PassableFoliage.MODID)
 public final class PassableFoliage {
 
-    public static final String MODID = "passablefoliage";
-    public static final String NAME = "Passable Foliage";
+	public static final String MODID = "passablefoliage";
+	public static final String NAME = "Passable Foliage";
 
-    public PassableFoliage() {
-        MinecraftForge.EVENT_BUS.addListener(PassableFoliage::tagsUpdated);
-    }
+	public PassableFoliage() {
+		MinecraftForge.EVENT_BUS.addListener(PassableFoliage::tagsUpdated);
+	}
 
-    public static void onEntityCollidedWithLeaves(World world, BlockPos pos, Entity entity) {
+	public static void onEntityCollidedWithLeaves(World world, BlockPos pos, Entity entity) {
 
-        if (!(entity instanceof LivingEntity)) {
-            return;
-        }
-        if (entity instanceof PlayerEntity) {
-            PlayerEntity player = ((PlayerEntity) entity);
-            if (player.isCreative() && player.abilities.isFlying) {
-                return;
-            }
-        }
+		if (!(entity instanceof LivingEntity)) {
+			return;
+		}
+		if (entity instanceof PlayerEntity) {
+			PlayerEntity player = ((PlayerEntity) entity);
+			if (player.isCreative() && player.abilities.isFlying) {
+				return;
+			}
+		}
 
-        LivingEntity livingEntity = (LivingEntity) entity;
+		LivingEntity livingEntity = (LivingEntity) entity;
 
-        if (!PassableFoliageCommonConfig.soundsPlayerOnly || entity instanceof PlayerEntity) {
-            // play a sound when an entity falls into leaves; do this before altering motion
-            if (livingEntity.fallDistance > 3f) {
-                entity.playSound(SoundEvents.BLOCK_GRASS_BREAK, SoundType.PLANT.getVolume() * 0.6f * PassableFoliageCommonConfig.soundVolume, SoundType.PLANT.getPitch() * 0.65f);
-            }
-            // play a sound when an entity is moving through leaves (only play sound every 6 ticks as to not flood sound events)
-            else if (world.getGameTime() % 6 == 0) {
-                double motion = entity.getMotion().lengthSquared();
-                if (motion > 5e-7) {
-                    entity.playSound(SoundEvents.BLOCK_GRASS_HIT, SoundType.PLANT.getVolume() * 0.5f * PassableFoliageCommonConfig.soundVolume, SoundType.PLANT.getPitch() * 0.45f);
-                }
-            }
-        }
+		if (!PassableFoliageCommonConfig.soundsPlayerOnly || entity instanceof PlayerEntity) {
+			// play a sound when an entity falls into leaves; do this before altering motion
+			if (livingEntity.fallDistance > 3f) {
+				entity.playSound(SoundEvents.BLOCK_GRASS_BREAK, SoundType.PLANT.getVolume() * 0.6f * PassableFoliageCommonConfig.soundVolume, SoundType.PLANT.getPitch() * 0.65f);
+			}
+			// play a sound when an entity is moving through leaves (only play sound every 6 ticks as to not flood sound events)
+			else if (world.getGameTime() % 6 == 0) {
+				double motion = entity.getMotion().lengthSquared();
+				if (motion > 5e-7) {
+					entity.playSound(SoundEvents.BLOCK_GRASS_HIT, SoundType.PLANT.getVolume() * 0.5f * PassableFoliageCommonConfig.soundVolume, SoundType.PLANT.getPitch() * 0.45f);
+				}
+			}
+		}
 
-        float h = 1, v = 1;
-        if (EnchantmentHelper.getMaxEnchantmentLevel(PassableFoliageRegistries.LEAF_WALKER, livingEntity) == 0 && livingEntity.getMotion().getY() <= 0) {
-            if (!world.isRemote) {
-                v = PassableFoliageCommonConfig.speedReductionVertical;
-            }
-            h = PassableFoliageCommonConfig.speedReductionHorizontal;
-        }
-        // reduce movement speed when inside of leaves, but allow players/mobs to jump out of them
-        if (h < 1 || v < 1) {
-            Vector3d newMotion = entity.getMotion().mul(h, v, h);
-            entity.setMotion(newMotion);
-        }
+		float h = 1, v = 1;
+		if (EnchantmentHelper.getMaxEnchantmentLevel(PassableFoliageRegistries.LEAF_WALKER, livingEntity) == 0 && livingEntity.getMotion().getY() <= 0) {
+			if (!world.isRemote) {
+				v = PassableFoliageCommonConfig.speedReductionVertical;
+			}
+			h = PassableFoliageCommonConfig.speedReductionHorizontal;
+		}
+		// reduce movement speed when inside of leaves, but allow players/mobs to jump out of them
+		if (h < 1 || v < 1) {
+			Vector3d newMotion = entity.getMotion().mul(h, v, h);
+			entity.setMotion(newMotion);
+		}
 
-        // modify falling damage when falling into leaves
-        if (livingEntity.fallDistance > PassableFoliageCommonConfig.fallDamageThreshold) {
-            livingEntity.fallDistance -= PassableFoliageCommonConfig.fallDamageThreshold;
-            EffectInstance pe = livingEntity.getActivePotionEffect(Effects.JUMP_BOOST);
-            int amount = MathHelper.ceil(livingEntity.fallDistance * PassableFoliageCommonConfig.fallDamageReduction * ((pe == null) ? 1.0f : 0.9f));
-            livingEntity.attackEntityFrom(DamageSource.FALL, amount);
-        }
+		// modify falling damage when falling into leaves
+		if (livingEntity.fallDistance > PassableFoliageCommonConfig.fallDamageThreshold) {
+			livingEntity.fallDistance -= PassableFoliageCommonConfig.fallDamageThreshold;
+			EffectInstance pe = livingEntity.getActivePotionEffect(Effects.JUMP_BOOST);
+			int amount = MathHelper.ceil(livingEntity.fallDistance * PassableFoliageCommonConfig.fallDamageReduction * ((pe == null) ? 1.0f : 0.9f));
+			livingEntity.attackEntityFrom(DamageSource.FALL, amount);
+		}
 
-        // reset fallDistance
-        if (livingEntity.fallDistance > 1f) {
-            livingEntity.fallDistance = 1f;
-        }
+		// reset fallDistance
+		if (livingEntity.fallDistance > 1f) {
+			livingEntity.fallDistance = 1f;
+		}
 
-        // Riding a mob won't protect you; Process riders last
-        if (entity.isBeingRidden()) {
-            for (Entity ent : entity.getPassengers()) {
-                onEntityCollidedWithLeaves(world, pos, ent);
-            }
-        }
+		// Riding a mob won't protect you; Process riders last
+		if (entity.isBeingRidden()) {
+			for (Entity ent : entity.getPassengers()) {
+				onEntityCollidedWithLeaves(world, pos, ent);
+			}
+		}
 
-    }
+	}
 
-    private static boolean updated;
+	private static boolean updated;
 
-    public static void tagsUpdated(TagsUpdatedEvent event) {
-        updated = true;
-    }
+	public static void tagsUpdated(TagsUpdatedEvent event) {
+		updated = true;
+		for (Block block : PassableFoliageTags.PASSABLES.getAllElements()) {
+			for (BlockState state : block.getStateContainer().getValidStates()) {
+				state.cacheState();
+			}
+		}
+		//Blocks.cacheBlockStates();
+	}
 
-    public static boolean isPassable(BlockState state) {
-        if (updated) {
-            return state.getBlock().isIn(PassableFoliageTags.PASSABLES);
-        } else {
-            return false;
-        }
-    }
+	public static boolean isPassable(BlockState state) {
+		if (updated) {
+			return state.getBlock().isIn(PassableFoliageTags.PASSABLES);
+		} else {
+			return false;
+		}
+	}
 }
---
+++ src/main/java/snownee/passablefoliage/mixin/MixinBlockStateCache.java
@@ -0,0 +1,43 @@
+package snownee.passablefoliage.mixin;
+
+import java.util.Arrays;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import snownee.passablefoliage.PassableFoliage;
+
+@Mixin(targets = "net.minecraft.block.AbstractBlock$AbstractBlockState$Cache")
+public class MixinBlockStateCache {
+
+	@Final
+	@Shadow
+	protected VoxelShape collisionShape;
+	@Final
+	@Shadow
+	protected boolean isCollisionShapeLargerThanFullBlock;
+	@Final
+	@Shadow
+	private boolean[] solidSides;
+	@Final
+	@Shadow
+	protected boolean opaqueCollisionShape;
+
+	@Inject(at = @At(value = "RETURN"), method = "<init>")
+	private void pfoliage_modifyCollisionShape(BlockState state, CallbackInfo info) {
+		if (PassableFoliage.isPassable(state)) {
+			collisionShape = VoxelShapes.empty();
+			isCollisionShapeLargerThanFullBlock = false;
+			Arrays.fill(solidSides, false);
+			opaqueCollisionShape = false;
+		}
+	}
+
+}
