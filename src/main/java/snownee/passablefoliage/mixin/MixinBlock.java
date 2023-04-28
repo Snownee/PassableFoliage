d36d41b432eb0c4b6558029c10451e659f757c15
Snownee <i@snownee.com>
2023-04-28 14:43:46 +0800

    fix: #38

---
+++ src/main/java/snownee/passablefoliage/mixin/MixinBlock.java
@@ -0,0 +1,22 @@
+package snownee.passablefoliage.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.world.level.block.Block;
+import snownee.passablefoliage.PassableFoliageBlock;
+
+@Mixin(Block.class)
+public class MixinBlock implements PassableFoliageBlock {
+
+	private boolean pfoliage$passable;
+
+	@Override
+	public void pfoliage$setPassable(boolean passable) {
+		pfoliage$passable = passable;
+	}
+
+	@Override
+	public boolean pfoliage$isPassable() {
+		return pfoliage$passable;
+	}
+}
---
+++ src/main/java/snownee/passablefoliage/mixin/MixinBlocks.java
@@ -0,0 +1,34 @@
+package snownee.passablefoliage.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import snownee.passablefoliage.CoreModule;
+import snownee.passablefoliage.PassableFoliageBlock;
+
+@Mixin(Blocks.class)
+public class MixinBlocks {
+
+	private static boolean err;
+
+	@Inject(method = "rebuildCache", at = @At("HEAD"))
+	private static void pfoliage_rebuildCache(CallbackInfo ci) {
+		for (Block block : Registry.BLOCK) {
+			try {
+				((PassableFoliageBlock) block).pfoliage$setPassable(block.builtInRegistryHolder().is(CoreModule.PASSABLES));
+			} catch (Throwable e) {
+				if (!err) {
+					System.err.println(e);
+					err = true;
+				}
+				((PassableFoliageBlock) block).pfoliage$setPassable(false);
+			}
+		}
+	}
+
+}
