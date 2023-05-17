7558e7b641ba4e1aa0419bd7f610a76d45045ff5
Snownee <i@snownee.com>
2023-05-17 10:20:31 +0800

    fix: higher priority

--- src/main/java/snownee/passablefoliage/mixin/BlocksMixin.java
+++ src/main/java/snownee/passablefoliage/mixin/BlocksMixin.java
@@ -11,7 +11,7 @@ import net.minecraft.world.level.block.Blocks;
 import snownee.passablefoliage.CoreModule;
 import snownee.passablefoliage.PassableFoliageBlock;
 
-@Mixin(Blocks.class)
+@Mixin(value = Blocks.class, priority = 500)
 public class BlocksMixin {
 
 	private static boolean err;
