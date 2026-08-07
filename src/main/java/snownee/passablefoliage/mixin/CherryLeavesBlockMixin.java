d21266ea3fe91bad72521b41da1f7d98eebeb165
Snownee <i@snownee.com>
2026-08-08 05:58:39 +0800

    improve: better support to modded vines (close #70)

--- src/main/java/snownee/passablefoliage/mixin/CherryLeavesBlockMixin.java
+++ src/main/java/snownee/passablefoliage/mixin/CherryLeavesBlockMixin.java
@@ -1,9 +1,9 @@
 package snownee.passablefoliage.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
@@ -15,32 +15,15 @@ import snownee.passablefoliage.PassableFoliage;
 @Mixin(CherryLeavesBlock.class)
 public class CherryLeavesBlockMixin {
 
-	@Inject(
-			method = "animateTick",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
-	private void pfoliage_animateTickPre(
+	@WrapMethod(method = "animateTick")
+	private void pfoliage_animateTick(
 			BlockState blockState,
 			Level level,
 			BlockPos blockPos,
 			RandomSource randomSource,
-			CallbackInfo ci) {
+			Operation<Void> original) {
 		PassableFoliage.setSuppressPassableCheck(true);
-	}
-
-	@Inject(
-			method = "animateTick",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;",
-					shift = At.Shift.AFTER))
-	private void pfoliage_animateTickPost(
-			BlockState blockState,
-			Level level,
-			BlockPos blockPos,
-			RandomSource randomSource,
-			CallbackInfo ci) {
+		original.call(blockState, level, blockPos, randomSource);
 		PassableFoliage.setSuppressPassableCheck(false);
 	}
 
