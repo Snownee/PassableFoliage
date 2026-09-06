a18fd3ca577db91b931497dfc61a35f0e9114a87
Snownee <i@snownee.com>
2026-09-07 05:02:29 +0800

    .

--- src/main/java/snownee/passablefoliage/mixin/fabric/WalkNodeEvaluatorMixin.java
+++ src/main/java/snownee/passablefoliage/mixin/fabric/WalkNodeEvaluatorMixin.java
@@ -4,32 +4,29 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathType;
 import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
+import snownee.passablefoliage.PassableFoliage;
 import snownee.passablefoliage.PassableFoliageCommonConfig;
-import snownee.passablefoliage.duck.PassableFoliageBlock;
 
 @Mixin(WalkNodeEvaluator.class)
 public class WalkNodeEvaluatorMixin {
 	@Inject(
 			method = "getPathTypeFromState",
-			at = @At(
-					value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z", shift = At.Shift.BEFORE),
-			locals = LocalCapture.CAPTURE_FAILSOFT,
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z"),
 			cancellable = true)
 	private static void pfoliage_useCorrectPathType(
-			BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<PathType> cir, BlockState state, Block block) {
-		if (!PassableFoliageCommonConfig.modifyPathFinding) {
-			return;
-		}
-		if (block instanceof PassableFoliageBlock passableFoliageBlock && passableFoliageBlock.pfoliage$isPassable() &&
-				state.getFluidState().isEmpty()) {
+			BlockGetter blockGetter,
+			BlockPos blockPos,
+			CallbackInfoReturnable<PathType> cir,
+			@Local BlockState state) {
+		if (!PassableFoliageCommonConfig.playerOnly && PassableFoliageCommonConfig.modifyPathFinding && PassableFoliage.isPassable(state)) {
 			cir.setReturnValue(PathType.LEAVES);
 		}
 	}
